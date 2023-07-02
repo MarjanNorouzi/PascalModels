3f275895172aedbf0f259a0b8b3ccdf9622b4a0f
Bagheri.H <Bagheri.H@test.com>
2023-07-02 10:46:32 +0330

    jabejayi validator nosanad

--- PascalModels/Validations/FromDateToDateReqValidator.cs
+++ PascalModels/Validations/FromDateToDateReqValidator.cs
@@ -35,4 +35,42 @@ namespace Personal_Information.Validators.SQLValidators
             return true;
         }
     }
+
+    public class FromSanadNoToNoReqValidator : AbstractValidator<FromSanadNoToNoReq>
+    {
+        public FromSanadNoToNoReqValidator()
+        {
+            When(p =>
+                      (!(string.IsNullOrWhiteSpace(p.NumFrom.ToString()) && string.IsNullOrWhiteSpace(p.NumTo.ToString()))
+                      && !(string.IsNullOrWhiteSpace(p.FromDate.ToString()) || string.IsNullOrWhiteSpace(p.ToDate.ToString()))),
+                      () =>
+                      {
+                          RuleFor(x => x).NotEmpty().WithMessage("only one of the fields must be filled");
+                      });
+
+            When(p =>
+                      (!(string.IsNullOrWhiteSpace(p.FromDate.ToString()) && string.IsNullOrWhiteSpace(p.ToDate.ToString())))
+                      && !(string.IsNullOrWhiteSpace(p.NumFrom.ToString()) || string.IsNullOrWhiteSpace(p.NumTo.ToString())),
+                      () =>
+                      {
+                          RuleFor(x => x).NotEmpty().WithMessage("only one of the fields must be filled");
+                      });
+
+            When(p =>
+                      (string.IsNullOrWhiteSpace(p.NumFrom.ToString()) || string.IsNullOrWhiteSpace(p.NumTo.ToString())),
+                      () =>
+                      {
+                          RuleFor(x => x).NotEmpty().WithMessage("both must be filled('FromNum' And 'ToNum').");
+                      });
+
+            When(p =>
+                      (p.NumFrom < p.NumTo),
+                      () =>
+                      {
+                          RuleFor(x => x).NotEmpty().WithMessage("the selected range is incorrect.");
+                      });
+
+
+        }
+    }
 }
