894c57d7063ea04245f6be11969d115897ba813e
Bagheri.H <Bagheri.H@test.com>
2023-07-02 09:55:01 +0330

    htgfh##

--- PascalModels/Models/GeneralsListReportByGrCodeReq.cs
+++ PascalModels/Models/GeneralsListReportByGrCodeReq.cs
@@ -1,7 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using PascalModels.Models.BaseModels;
+using System.ComponentModel.DataAnnotations;
 
-public class GeneralsListReportByGrCodeReq : RemainedAccountReq
+public class GeneralsListReportByGrCodeReq : RemainedAccReq
 {
     [StringLength(2)]
     public string GrCode { get; set; }
+}
+
+public class GeneralsListReportByGrCodeRes : DebitCreditSum
+{
+    public int GenCode { get; set; }
+    public int GenName { get; set; }
 }
