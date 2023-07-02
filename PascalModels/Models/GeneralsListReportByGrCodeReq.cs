37a42ab31555b75fdb4eb1a537cd1402a5d2d4d9
Bagheri.H <Bagheri.H@test.com>
2023-07-02 09:43:37 +0330

    hi

---
+++ PascalModels/Models/GeneralsListReportByGrCodeReq.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class GeneralsListReportByGrCodeReq : RemainedAccountReq
+{
+    [StringLength(2)]
+    public string GrCode { get; set; }
+}
---
+++ PascalModels/Validations/FromNumToNumReqValidator.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+
+namespace Personal_Information.Validators.SQLValidators
+{
+    public class FromNumToNumReqValidator : AbstractValidator<FromNumToNumReq>
+    {
+        public FromNumToNumReqValidator()
+        {
+            When(p =>
+                      (!(string.IsNullOrWhiteSpace(p.FromNum.ToString()) && string.IsNullOrWhiteSpace(p.ToNum.ToString()))
+                      && !(string.IsNullOrWhiteSpace(p.FromDate.ToString()) || string.IsNullOrWhiteSpace(p.ToDate.ToString()))),
+                      () =>
+                      {
+                          RuleFor(x => x).NotEmpty().WithMessage("only one of the fields must be filled");
+                      });
+
+            When(p =>
+                      (!(string.IsNullOrWhiteSpace(p.FromDate.ToString()) && string.IsNullOrWhiteSpace(p.ToDate.ToString())))
+                      && !(string.IsNullOrWhiteSpace(p.FromNum.ToString()) || string.IsNullOrWhiteSpace(p.ToNum.ToString())),
+                      () =>
+                      {
+                          RuleFor(x => x).NotEmpty().WithMessage("only one of the fields must be filled");
+                      });
+
+            When(p =>
+                      (string.IsNullOrWhiteSpace(p.FromNum.ToString()) || string.IsNullOrWhiteSpace(p.ToNum.ToString())),
+                      () =>
+                      {
+                          RuleFor(x => x).NotEmpty().WithMessage("both must be filled('FromNum' And 'ToNum').");
+                      });
+
+            When(p =>
+                      (p.FromNum == null || p.ToNum == null ),
+                      () =>
+                      {
+                          RuleFor(x => x).NotEmpty().WithMessage("both must be filled('FromNum' And 'ToNum').");
+                      });
+
+            When(p =>
+                      (p.FromNum < p.ToNum),
+                      () =>
+                      {
+                          RuleFor(x => x).NotEmpty().WithMessage("the selected range is incorrect.");
+                      });
+
+            
+        }
+    }
+}
