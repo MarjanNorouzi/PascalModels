e7e9181b7223e212fef56201d0d912c02798977b
Bagheri.H <Bagheri.H@test.com>
2023-07-02 10:29:54 +0330

    add models ( DetailsListBySubCodeReq ,ReferReportReq,TurnoverBySubCodeReq)

---
+++ PascalModels/Models/DetailsListBySubCodeReq.cs
@@ -0,0 +1,21 @@
+﻿using PascalModels.Models.BaseModels;
+using System.ComponentModel.DataAnnotations;
+
+public class DetailsListBySubCodeReq : RemainedAccReq
+{
+    [StringLength(10)]
+    public string SubCode { get; set; }
+}
+
+public class DetailsListBySubCodeRes : DebitCreditSum
+{
+    [StringLength(8)]
+    public string DetCode { get; set; }
+
+    [StringLength(50)]
+    public string DetName { get; set; }
+}
+
+public class DetailsListBySubCodeTotalRes : TotalRemained
+{
+}
---
+++ PascalModels/Models/TurnoverBySubCodeReq.cs
@@ -0,0 +1,20 @@
+﻿using PascalModels.Models.BaseModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace Personal_Information.Models.ProcModels
+{
+    public class TurnoverBySubCodeReq : FromSanadNoToNoReq
+    {
+        [StringLength(10)]
+        public string SubCode { get; set; }
+    }
+
+    public class TurnoverBySubCodeRes : ReferReportRes
+    {
+        public decimal Remained { get; set; }
+    }
+
+    public class TurnoverBySubCodeTotalRes : ReferReportTotalRes
+    {
+    }
+}
