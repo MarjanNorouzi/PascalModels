6c104967163bd38377fe9696482151d6dd6b6dc2
Programmer <Programmer@DESKTOP-KTGMGOS>
2023-07-03 12:51:44 +0330

    Edit insert validators

--- PascalModels/Validations/InsertDetReqValidator.cs
+++ PascalModels/Validations/InsertDetReqValidator.cs
@@ -7,8 +7,17 @@ namespace PascalModels.Validations
     {
         public InsertDetReqValidator()
         {
-            RuleFor(x => x.DetCode).NotEmpty().NotNull().WithMessage("لطفا کد تفصیل را وارد کنید").MaximumLength(6).WithMessage("بیش از 8 کاراکتر مجاز نمی باشد");
-            RuleFor(x => x.DetName).NotEmpty().NotNull().WithMessage("لطفا نام تفصیل را وارد کنید").MaximumLength(50).WithMessage("بیش از 50 کاراکتر مجاز نمی باشد");
+            RuleFor(x => x.DetCode).NotEmpty().NotNull().WithMessage("لطفا کد تفصیل را وارد کنید")
+                                   .MaximumLength(6).WithMessage("بیش از 8 کاراکتر مجاز نمی باشد")
+                                   .Must(ValidateCode).WithMessage("مقدار کد تفصیل فقط می تواند عدد صحیح باشد");
+
+            RuleFor(x => x.DetName).NotEmpty().NotNull().WithMessage("لطفا نام تفصیل را وارد کنید")
+                                   .MaximumLength(50).WithMessage("بیش از 50 کاراکتر مجاز نمی باشد");
+        }
+
+        private bool ValidateCode(string Code)
+        {
+            return int.TryParse(Code, out int _);
         }
     }
 }
--- PascalModels/Validations/InsertGenReqValidator.cs
+++ PascalModels/Validations/InsertGenReqValidator.cs
@@ -7,9 +7,22 @@ namespace PascalModels.Validations
     {
         public InsertGenReqValidator()
         {
-            RuleFor(x => x.GroupCode).NotEmpty().NotNull().WithMessage("لطفا کد گروه را وارد کنید").MaximumLength(2).WithMessage("بیش از 2 کاراکتر مجاز نمی باشد");
-            RuleFor(x => x.GenCode).NotEmpty().NotNull().WithMessage("لطفا کد کل را وارد کنید").MaximumLength(6).WithMessage("بیش از 6 کاراکتر مجاز نمی باشد");
-            RuleFor(x => x.GenName).NotEmpty().NotNull().WithMessage("لطفا نام کل را وارد کنید").MaximumLength(50).WithMessage("بیش از 50 کاراکتر مجاز نمی باشد");
+            RuleFor(x => x.GroupCode).NotEmpty().NotNull().WithMessage("لطفا کد گروه را وارد کنید")
+                                     .MaximumLength(2).WithMessage("بیش از 2 کاراکتر مجاز نمی باشد")
+                                     .Must(ValidateCode).WithMessage("مقدار کد گروه فقط می تواند عدد صحیح باشد");
+
+
+            RuleFor(x => x.GenCode).NotEmpty().NotNull().WithMessage("لطفا کد کل را وارد کنید")
+                                   .MaximumLength(6).WithMessage("بیش از 6 کاراکتر مجاز نمی باشد")
+                                   .Must(ValidateCode).WithMessage("مقدار کد کل فقط می تواند عدد صحیح باشد");
+
+            RuleFor(x => x.GenName).NotEmpty().NotNull().WithMessage("لطفا نام کل را وارد کنید")
+                                   .MaximumLength(50).WithMessage("بیش از 50 کاراکتر مجاز نمی باشد");
+        }
+
+        private bool ValidateCode(string Code)
+        {
+            return int.TryParse(Code, out int _);
         }
     }
 }
